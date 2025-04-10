c3d767f3f207b8680a87566de899cd7ad5e03ec5
liudmylatymoshenko <timlv7777@gmail.com>
2025-04-10 17:45:10 +0300

    Added assets

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,12 @@
+class Assets {
+  Assets._();
+
+  static const _iconsPath = 'assets/icons';
+
+  static const loader = '$_iconsPath/loader.svg';
+  static const moon = '$_iconsPath/moon.svg';
+  static const sun = '$_iconsPath/sun.svg';
+  static const search = '$_iconsPath/search.svg';
+  static const star = '$_iconsPath/star.svg';
+  static const arrowLeft = '$_iconsPath/arrowLeft.svg';
+}
